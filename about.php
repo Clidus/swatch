cb08f358a4b3b0ab71e1cee28a21524baf813708
Clidus <josh@clidus.com>
2014-04-10 22:05:17 +0100

    Add logo

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 ?>
 	<div class="container">
 		<h1>About Swatch Internet Time</h1>
-		
+
 		<p><a href="http://en.wikipedia.org/wiki/Swatch_Internet_Time" target="_blank">Swatch Internet Time</a> is a decimal time system created in 1998 by the <a href="http://www.swatch.com/gb_en/internettime/" target="_blank">Swatch corporation</a>. While adoption of Swatch Internet Time was a complete failure it is in fact the perfect method of keeping time. An Earth day is divided into 1000 parts, known as “beats”. Swatch is zero based, has no time zones and doesn’t observe the bullshit that is daylights saving.</p> 
 		<p>Swatch time is perfectly designed for the global community as it is the same across the globe. Need to arrange a meeting with a colleague on the other side of the planet? Book it in Swatch time and no one will ever be confused about when it is.</p>
 		<p>For logical reasons the start of the day in Swatch Internet Time is midnight in Biel, Switzerland (UTC+1).</p>
@@ -12,6 +12,8 @@
 		<p>Website created by <a href="http://www.clidus.com/" target="_blank">Joshua Marketis</a>.</p>
 
 		<p><a href="https://github.com/Clidus/swatch" target="_blank">Source code on GitHub</a>.</p> 
+
+		<center><img src="images/swatch.png" alt="Swatch Internet Time" title="Swatch Internet Time" /></center>
 	</div>
 
 	<script src="js/jquery-2.0.3.min.js"></script>
