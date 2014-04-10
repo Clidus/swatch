d8dfb30d726aa5a18fe3064da0e446d9436514aa
Clidus <josh@clidus.com>
2014-04-10 21:44:14 +0100

    Fix tabbing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // calculate the time of the future
 function GetInternetTime() {
-	// get date in UTC/GMT
-  	var date = new Date();
+    // get date in UTC/GMT
+    var date = new Date();
     var hours = date.getUTCHours();
     var minutes = date.getUTCMinutes();
     var seconds = date.getUTCSeconds();
@@ -16,16 +16,16 @@ function GetInternetTime() {
     var secondsInABeat = 86.4;
 
     // calculate beats to two decimal places
-	var beats = Math.abs(timeInSeconds / secondsInABeat).toFixed(2);
+    var beats = Math.abs(timeInSeconds / secondsInABeat).toFixed(2);
 
-	// update page
-	$('#swatchTime').html(beats);
+    // update page
+    $('#swatchTime').html(beats);
 }
 
 $(function() {
-	// display at page load
-	GetInternetTime();
+    // display at page load
+    GetInternetTime();
 
-	// calculate every second
-	setInterval(GetInternetTime, 1000);
+    // calculate every second
+    setInterval(GetInternetTime, 1000);
 });
