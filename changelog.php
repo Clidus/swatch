20bd0af9949cf99cf3e93b9fa7064b67d2aac716
Clidus <josh@clidus.com>
2014-04-22 20:56:24 +0100

    Incorrect date in changelog

--- changelog.php
+++ changelog.php
@@ -5,7 +5,7 @@
 		<h1>Changelog</h1>
 		
 		<div>
-			<p><b>Version 1.2.0 - 2014-04-15</b></p>
+			<p><b>Version 1.2.0 - 2014-04-22</b></p>
 			<ul>
 				<li><b>Feature:</b> Live Unix/Epoch Time clock.</li>
 			</ul>
